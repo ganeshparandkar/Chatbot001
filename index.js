1f1936ff25e8acc5679d6766229ccece7fb2a6cd
ganeshparandkar <ganeshparandkar@gmail.com>
2024-05-02 20:03:51 +0530

    getting req data

--- index.js
+++ index.js
@@ -11,14 +11,20 @@ app.get('/webhook',(req,res)=>{
 })
 
 app.post('/webhook',(req,res)=>{
-    console.log(`REQ: ${req}`)
-    const data = req.body.payload.payload;
-    const userData = req.body.payload;
-    const inputdata = data.text;
-    const queryParams = req;
-    console.log(queryParams)
-    res.send(`Hello, this is a POST request to my webhook. data: ${data}\n\ninputdata:${inputdata}\n
-    \n Userdata:${userData}`);
+    console.log('Request Method:', req.method);
+    console.log('Request URL:', req.url);
+    console.log('Query Parameters:', req.query);
+    console.log('Request Headers:', req.headers);
+    console.log('Request Body:', req.body);
+
+    const ReqData = {
+        method: req.method,
+        url: req.url,
+        queryParameters: req.query,
+        headers: req.headers,
+        body: req.body
+    };
+    res.send(`Hello, this is a POST request to my webhook. data: ${ReqData}`);
 });
 // Webhook endpoint for receiving messages
 
