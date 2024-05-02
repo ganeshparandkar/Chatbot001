c4611425bf482c429f0e0bdac5d55c8487ee10c6
ganeshparandkar <ganeshparandkar@gmail.com>
2024-05-02 19:54:19 +0530

    printing req

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.get('/webhook',(req,res)=>{
 })
 
 app.post('/webhook',(req,res)=>{
+    console.log(`REQ: ${req}`)
     const data = req.body.payload.payload;
     const userData = req.body.payload;
     const inputdata = data.text;
