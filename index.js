fb5ed88339e025c0adbbccbaa487f24e1dadc18f
ganeshparandkar <ganeshparandkar@gmail.com>
2024-05-02 19:45:24 +0530

    checking userdata according to old chatbot

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ app.get('/webhook',(req,res)=>{
 })
 
 app.post('/webhook',(req,res)=>{
+    const data = req.body.payload.payload;
+    const userData = req.body.payload;
+    const inputdata = data.text;
     const queryParams = req;
     console.log(queryParams)
-    res.send(`Hello, this is a POST request to my webhook. Query parameters: ${queryParams}`);
+    res.send(`Hello, this is a POST request to my webhook. data: ${data}\n\ninputdata:${inputdata}\n
+    \n Userdata:${userData}`);
 });
 // Webhook endpoint for receiving messages
 
